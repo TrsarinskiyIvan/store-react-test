46ce05eaf42c5a8e9171c76f3f5890b8497750c5
Ivan Tsarinskiy <itsarinskiy@gmail.com>
2022-10-11 23:38:29 +0300

    css correction

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -8,7 +8,6 @@ export default class Card extends PureComponent {
             <div className="card">
                 <img className="card__pic" src={this.props.imgUrl} alt={this.props.title} />
                 <h2 className="card__title">{this.props.title}</h2>
-                <h2 className="card__title">{this.props.idProduct}</h2>
                 <p className="card__price">{this.props.selectedCourse}&nbsp;{this.props.price}</p>
                 <div className="card__to-cart-block"><img src="./to-cart.png" alt="" /></div>
             </div>
