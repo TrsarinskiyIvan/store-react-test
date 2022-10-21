f110db521eaddc4afc6e35ddb9b3a17aad16697c
Ivan Tsarinskiy <itsarinskiy@gmail.com>
2022-10-21 11:35:51 +0300

    changed CourseMenuWithData

--- src/components/course-menu/CourseMenuWithData.jsx
+++ src/components/course-menu/CourseMenuWithData.jsx
@@ -1,7 +1,7 @@
 import { PureComponent } from "react"
 import DataProvider from "../data-provider/DataProvider"
-import { CURRENCIES } from '../../helper/queries';
-import CourseMenu from "./CourseMenu";
+import { CURRENCIES } from '../../helper/queries'
+import CourseMenu from "./CourseMenu"
 
 export default class CourseMenuWithData extends PureComponent {
     render() {
